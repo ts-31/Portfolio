f56dc8361f90a289967950e66527d0413755e703
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-04-03 20:20:30 +0530

    Fixing techstack.js issue

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,13 +1,11 @@
-// Techstack.js
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
   DiPython,
-  DiGit
+  DiGit,
 } from "react-icons/di";
 import {
   SiRedis,
